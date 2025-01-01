41cf09c017cd4de02929e7607002cb965399064e
Brandon Youngquist <brandon@youngquist.dev>
2025-01-01 00:22:51 -0600

    migration

---
+++ prisma/migrations/20250101020723_migrations/migration.sql
@@ -0,0 +1,23 @@
+-- AlterTable
+ALTER TABLE "Address" ALTER COLUMN "createdAt" SET DEFAULT NOW();
+
+-- AlterTable
+ALTER TABLE "Item" ALTER COLUMN "createdAt" SET DEFAULT NOW();
+
+-- AlterTable
+ALTER TABLE "Location" ALTER COLUMN "createdAt" SET DEFAULT NOW();
+
+-- AlterTable
+ALTER TABLE "Organization" ALTER COLUMN "createdAt" SET DEFAULT NOW();
+
+-- AlterTable
+ALTER TABLE "Price" ALTER COLUMN "createdAt" SET DEFAULT NOW();
+
+-- AlterTable
+ALTER TABLE "Store" ALTER COLUMN "createdAt" SET DEFAULT NOW();
+
+-- AlterTable
+ALTER TABLE "Unit" ALTER COLUMN "createdAt" SET DEFAULT NOW();
+
+-- AlterTable
+ALTER TABLE "User" ALTER COLUMN "createdAt" SET DEFAULT NOW();
